d5356e0e6468b1bd02d0fe47b0135a3ea501e8db
Raul Martin <raulmartinleva@hotmail.com>
2019-12-02 13:36:11 +0100

    Commit ultimos cambios

--- app/src/main/java/com/example/where/SplashActivity.java
+++ app/src/main/java/com/example/where/SplashActivity.java
@@ -23,7 +23,7 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(this, MainActivity.class);
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
             }
